b432d5dd99431a24855159692f95e3b79a142015
unknown <rafanetow@gmail.com>
2020-05-16 22:50:39 -0600

    configuration of DB

--- app/models/movieModel.js
+++ app/models/movieModel.js
@@ -1,3 +1,18 @@
 module.exports = (sequelize,  Sequlize) => {
-     
+    const userMovie = sequelize.define("user", {
+        email: {
+            type: Sequlize.STRING
+        },
+        auth0Id: {
+            type: Sequlize.STRING
+        },
+        typeOfMovies: {
+            type: Sequlize.STRING
+        },
+        yearOfMovies: {
+            type: Sequlize.STRING
+        }
+
+    });
+    return userMovie; 
 }
