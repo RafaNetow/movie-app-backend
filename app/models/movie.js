7496fbd344864735a9966b1f0c5426bc53a9cdbc
unknown <rafanetow@gmail.com>
2020-05-19 00:12:25 -0600

    adding autoincremental id

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize,  Sequlize) => {
     const userMovie = sequelize.define("userMovie", {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
         email: {
             type: Sequlize.STRING
         },
